68ae8780fb55498efe7cba4d5815839a2878be90
WANG Xuerui <git@xen0n.name>
2024-09-07 14:21:21 +0800

    allow easy local testing with ignored .envrc and /test
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- _config.sh
+++ _config.sh
@@ -1,3 +1,5 @@
+[[ -e .envrc ]] && . .envrc
+
 ROOTFS_IMAGE_TAG="ghcr.io/loongson-community/dotnet-unofficial-build-rootfs:e0a68f5b86f8-20240904T171513Z"
 
 DOTNET_RUNTIME_BRANCH="v9.0.0-preview.7.24405.7"
