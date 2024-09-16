51144bf023004516e546c5591b10a637419ff569
WANG Xuerui <git@xen0n.name>
2024-09-16 19:03:04 +0800

    containers/builder: updates
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- containers/builder/build.sh
+++ containers/builder/build.sh
@@ -19,17 +19,36 @@ PACKAGES=(
     lld-18
     llvm-18
 
+    # host build deps
+    # this is mostly the same as, and should stay sync with those of cross
+    # rootfs
+    libcurl4-openssl-dev
+    libicu-dev
+    libkrb5-dev
     liblttng-ust-dev
+    libnuma-dev
+    libomp5
+    libomp-dev
+    libssl-dev
+    libunwind8-dev
     zlib1g-dev
 
-    # for dotnet-unofficial-build self use (pulling back the rootfs image)
-    docker.io
+    # optional deps
+    ninja-build
+
+    # for dotnet-unofficial-build self use
+    ccache  # to enable local build caches
+    docker.io  # to pull back the rootfs image in container form
+    sccache  # to enable potential caching with object storage backend
+    sudo  # to enable unprivileged builds
 )
 
 apt-get update
 apt-get install -y --no-install-recommends "${PACKAGES[@]}"
 locale-gen en_US.UTF-8
 
+# we might not refresh nodejs-built artifacts in our process, but having it
+# in place is more future-proof
 apt-get autoremove -y nodejs
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
@@ -39,4 +58,12 @@ apt-get clean
 cat > /README.txt <<READMEEOF
 # loongson-community/dotnet-unofficial-build builder image
 BUILD_TIMESTAMP=$BUILD_TIMESTAMP
+BUILDER_UID=$BUILDER_UID
+BUILDER_USER=$BUILDER_USER
 READMEEOF
+
+useradd -m -u "$BUILDER_UID" -U "$BUILDER_USER"
+echo "$BUILDER_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/builder
+chmod 0640 /etc/sudoers.d/builder
+
+rm "${BASH_SOURCE[0]}" || true
