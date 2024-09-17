94cd66c190e90cde22e6e5aa59c73c4c358405d0
WANG Xuerui <git@xen0n.name>
2024-09-17 17:40:14 +0800

    containers/builder: conform to VMR build prereqs
    Install additional deps according to
    https://github.com/dotnet/dotnet-buildtools-prereqs-docker/blob/8bb52e2a2cc565f932ca7fb76abd9a64d0c78018/src/debian/12/gcc14/amd64/Dockerfile.
    
    This is built as tag
    `ghcr.io/loongson-community/dotnet-unofficial-build-builder:20240917T082913Z`,
    and is confirmed to fix VMR Stage 1 builds (for x64).
    
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- containers/builder/build.sh
+++ containers/builder/build.sh
@@ -5,13 +5,7 @@ set -e
 PACKAGES=(
     build-essential
     ca-certificates
-    git
     g++-loongarch64-linux-gnu  # for lack of crossbuild-essential-loong64
-    cmake
-    curl
-    locales
-    python3
-    python3-libxml2
     wget
 
     # llvm/clang utils
@@ -19,12 +13,31 @@ PACKAGES=(
     lld-18
     llvm-18
 
+    # PowerShell install deps
+    apt-transport-https
+    software-properties-common
+
     # host build deps
+    azure-cli
+    cmake
+    curl
+    gdb
+    git
+    iputils-ping
+    locales
+    locales-all
+    lttng-tools
+    python3-dev
+    python3-pip
+    sudo
+    tzdata
+
     # this is mostly the same as, and should stay sync with those of cross
     # rootfs
     libcurl4-openssl-dev
     libicu-dev
     libkrb5-dev
+    liblldb-dev
     liblttng-ust-dev
     libnuma-dev
     libomp5
@@ -40,18 +53,29 @@ PACKAGES=(
     ccache  # to enable local build caches
     docker.io  # to pull back the rootfs image in container form
     sccache  # to enable potential caching with object storage backend
-    sudo  # to enable unprivileged builds
 )
 
 apt-get update
 apt-get install -y --no-install-recommends "${PACKAGES[@]}"
 locale-gen en_US.UTF-8
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
 # we might not refresh nodejs-built artifacts in our process, but having it
 # in place is more future-proof
 apt-get autoremove -y nodejs
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
+
+# install powershell
+#
+# from https://github.com/dotnet/dotnet-buildtools-prereqs-docker/blob/8bb52e2a2cc565f932ca7fb76abd9a64d0c78018/src/debian/12/gcc14/amd64/Dockerfile:
+#
+# > Specifically use deb 11 PMC because 12 doesn't have it yet.
+curl -sL https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -o packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get update
+apt-get install -y powershell
 
 apt-get clean
 
