173864dd36176ea5c8465dc50ba1fbcf4346d403
WANG Xuerui <git@xen0n.name>
2024-12-05 10:49:26 +0800

    fix: try to fix apparent lack of flags application
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- _config.ci.sh
+++ _config.ci.sh
@@ -26,9 +26,9 @@ ROOTFS_MUSL_IMAGE_TAG="$(cat "$(dirname "${BASH_SOURCE[0]}")"/rootfs-musl-image-
 
 # For the dotnet build system
 # see https://github.com/dotnet/runtime/issues/35727
-STAGE2_EXTRA_CFLAGS="-O2 -pipe -march=la64v1.0 -mtls-dialect=desc"
-STAGE2_EXTRA_CXXFLAGS="$STAGE2_EXTRA_CFLAGS"
 STAGE2_EXTRA_LDFLAGS="-Wl,-O1 -Wl,--as-needed -Wl,-z,pack-relative-relocs -Wl,--hash-style=gnu"
+STAGE2_EXTRA_CFLAGS="-O2 -pipe -march=la64v1.0 -mtls-dialect=desc $STAGE2_EXTRA_LDFLAGS"
+STAGE2_EXTRA_CXXFLAGS="$STAGE2_EXTRA_CFLAGS"
 
 # it may be better to align with dotnet upstream that still sticks with gzip
 # so far
