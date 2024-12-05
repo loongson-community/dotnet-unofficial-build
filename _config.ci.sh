715d2908754868a0e23a3c99d3a1f9b1b6e61d44
WANG Xuerui <git@xen0n.name>
2024-12-05 16:59:52 +0800

    ci: build https://github.com/loongson-community/dotnet tag v9.0.101+loong.20241205
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- _config.ci.sh
+++ _config.ci.sh
@@ -18,7 +18,7 @@ fi
 # the tag/branch in the CI task definition
 DOTNET_VMR_CHECKED_OUT=false
 DOTNET_VMR_CHECKOUT="/vmr"
-DOTNET_VMR_BRANCH="v9.0.0+loong.20241205"
+DOTNET_VMR_BRANCH="v9.0.101+loong.20241205"
 DOTNET_VMR_REPO=https://github.com/loongson-community/dotnet.git
 
 ROOTFS_GLIBC_IMAGE_TAG="$(cat "$(dirname "${BASH_SOURCE[0]}")"/rootfs-glibc-image-tag.txt)"
